cfeca4916aba6ed6a7760209e1ee4845e94afbe3
Sairam BN <bnsairam14@gmail.com>
2026-08-03 17:15:33 +0530

    Remove favicon link from head

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -125,7 +125,6 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     links: [
       { rel: "stylesheet", href: appCss },
       { rel: "canonical", href: SITE_URL },
-      { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
